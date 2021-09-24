7d86bfd6f374ddce7269465809cd36adf742898a
นาย ชาตรี แซ่ซ่ง <60030213+chatreezoo@users.noreply.github.com>
2021-09-24 13:31:34 +0700

    add homepage

--- front-end/src/components/homePage/BoxPage.js
+++ front-end/src/components/homePage/BoxPage.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './BoxPage.css'
-const BoxPage = ({title, subtitle}) => {
+const BoxPage = ({title, subtitle,pi}) => {
     return (
         <div className='box__page' >
-            {/* <img src={pic} alt="" /> */}
+            <img src={pi} alt="" />
             <h5>{title}</h5>
             <p>{subtitle}</p>
         </div>
