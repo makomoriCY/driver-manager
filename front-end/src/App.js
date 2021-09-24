2d01c2efa4537c988ae4837dfeac646373b3fd45
นาย ชาตรี แซ่ซ่ง <60030213+chatreezoo@users.noreply.github.com>
2021-09-24 12:18:30 +0700

    test

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import HomePage from "./components/homePage/HomePage";
 import Login from "./components/login/Login";
 import Register from "./components/login/Register";
 
 function App() {
   return (
     <div className="app">
-      <Login />
+      {/* <Login /> */}
       {/* <Register /> */}
+      <HomePage />
     </div>
   );
 }
