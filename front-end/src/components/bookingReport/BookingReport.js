bd2ba0a5d66dd853ecd7d489b2690ab3caeed11f
นาย ชาตรี แซ่ซ่ง <60030213+chatreezoo@users.noreply.github.com>
2021-10-03 09:56:20 +0700

    add Booking from

--- front-end/src/components/bookingReport/BookingReport.js
+++ front-end/src/components/bookingReport/BookingReport.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./BookingReport.css";
 
 const BookingReport = () => {
   return (
-    <div>
-      zxvcbvnb,m
-    </div>
-  )
-}
+    <div className="background___page">
+      <h1>รายงานการจอง</h1>
 
-export default BookingReport
+      <div className="BOX__Book">
+        <p>รายการ</p>
+        <table>
+        <tr>
+              <th>วัดถุประสงค์</th>
+              <th>สถานที่</th>
+              <th>วันเวลา</th>
+              <th>คนขับ</th>
+            </tr>
+            <tr>
+              <td>ดาวอังคาร</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+              <td>ไก่</td>
+            </tr>           
+        </table>
+        <textarea
+            rows="8"/>
+      </div>
+
+      <Link to="/">
+        <button className="Btn__back">ย้อนกลับ</button>
+      </Link>
+    </div>
+  );
+};
+
+export default BookingReport;
--- front-end/src/components/contact/Contact.js
+++ front-end/src/components/contact/Contact.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>
-      zxcfvgbh
+    <div className="background___page">
+      <h1>ติดต่อเรา</h1>
+      <div className="Box">
+        <p>8;k,</p>
+      </div>
+      <Link to="/">
+        <button className="Btn__back">ย้อนกลับ</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
