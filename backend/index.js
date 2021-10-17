969a9bd458037ea3421edf2a4e3e8de9731ea2f4
makomoriCY <chayootpong.bao@innosoft.ac.th>
2021-10-17 22:43:54 +0700

    setting index file

--- backend/index.js
+++ backend/index.js
@@ -2,3 +2,27 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
+
+// ตั้งค่า project
+const app = express()
+// ดึงค่า port จาก file env
+const port = process.env.PORT || 8080
+
+// ตั้งค่าฐานข้อมูล
+const uri = process.env.ATLAS_URI
+
+mongoose.connect(uri, {
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+const connection = mongoose.connection
+connection.once('open', () => console.log('DB connect'))
+
+// Middleware
+app.use(express.json())
+app.use(cors())
+
+
+app.listen(port, () => console.log(`Running on port:${port}`))
