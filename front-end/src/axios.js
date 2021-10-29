9bbfa4c478068b7533b496955f53db54e17e8d5d
นาย ชาตรี แซ่ซ่ง <60030213+chatreezoo@users.noreply.github.com>
2021-10-30 02:37:33 +0700

    change url backend

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL:'http://localhost:8080'
+    baseURL:'https://git.heroku.com/driver-backend1.git'
 })
 
 export default instance
