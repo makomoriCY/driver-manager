ed65bdc2e07098d521a7d977078e3acbbee45391
นาย ชาตรี แซ่ซ่ง <60030213+chatreezoo@users.noreply.github.com>
2021-10-30 02:54:29 +0700

    add header backned

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,39 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
-
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // ตั้งค่า project
-const app = express()
+const app = express();
 // ดึงค่า port จาก file env
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
+app.use((req, res, next) => {
+res.header("Access-Control-Allow-Origin", "*");
+res.header(
+    "Access-Control-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+);
+next();
+});
 
 // Middleware
-app.use(express.json())
-app.use(cors())
-
+app.use(express.json());
+app.use(cors());
 
 // ตั้งค่าฐานข้อมูล
-const uri = process.env.ATLAS_URI
+const uri = process.env.ATLAS_URI;
 
 mongoose.connect(uri, {
-    // useCreateIndex:true,
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
+  // useCreateIndex:true,
+useNewUrlParser: true,
+useUnifiedTopology: true,
+});
 
-const connection = mongoose.connection
-connection.once('open', () => console.log('DB connect'))
-
-
+const connection = mongoose.connection;
+connection.once("open", () => console.log("DB connect"));
 
 // Route
-const scheduleRouter = require('./routes/ScheduleRoute')
-app.use('/schedule', scheduleRouter)
+const scheduleRouter = require("./routes/ScheduleRoute");
+app.use("/schedule", scheduleRouter);
 
-
-app.listen(port, () => console.log(`Running on port:${port}`))
+app.listen(port, () => console.log(`Running on port:${port}`));
