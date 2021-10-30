d5fdbf07ef2a372a1ff87990c3d9fe223c30f619
นาย ชาตรี แซ่ซ่ง <60030213+chatreezoo@users.noreply.github.com>
2021-10-30 12:40:55 +0700

    edit send message

--- backend/routes/ScheduleRoute.js
+++ backend/routes/ScheduleRoute.js
@@ -69,7 +69,7 @@ router.delete("/:id", (req, res) => {
 });
 
 function sendnotification(name, department, type, approve) {
-  var sendData = `ชื่อผู้จอง : ${name} แผนก: ${department} ประเภทรถ: ${type} คนอนุมัติ: ${approve} `;
+  var sendData = `ชื่อผู้จอง : ${name}\n แผนก: ${department}\n ประเภทรถ: ${type}\n คนอนุมัติ: ${approve} `;
   var token = "QHNr1EB1MI3MTfexVmZJ6XQZFdBrOrGX174DZW1y3W7";
   var message = sendData;
 
