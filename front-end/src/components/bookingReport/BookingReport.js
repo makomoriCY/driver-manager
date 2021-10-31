cd14c1412f7c2d2c4d1b82a9ac0fcc7c1acc6730
makomoriCY <chayootpong.bao@innosoft.kmutt.ac.th>
2021-10-31 21:18:09 +0700

    format time

--- front-end/src/components/bookingReport/BookingReport.js
+++ front-end/src/components/bookingReport/BookingReport.js
@@ -12,7 +12,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import * as dayjs from 'dayjs'
+import dayjs from 'dayjs'
 
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
