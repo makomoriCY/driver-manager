e93f79d44a7cffae8e37dc682e63cd42b4d17568
makomoriCY <chayootpong.bao@innosoft.kmutt.ac.th>
2021-11-11 18:59:37 +0700

    set data function delete

--- backend/routes/ScheduleRoute.js
+++ backend/routes/ScheduleRoute.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const router = express.Router();
-const scheduleModel = require("../model/Schedule");
-var request = require("request");
+const express = require('express')
+const router = express.Router()
+const scheduleModel = require('../model/Schedule')
+var request = require('request')
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   scheduleModel
     .find()
-    .then((data) => res.json(data))
-    .catch((err) => res.status(404).json(`error:${err}`));
-});
+    .then(data => res.json(data))
+    .catch(err => res.status(404).json(`error:${err}`))
+})
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   const schedule = new scheduleModel({
     startDate: req.body.startDate,
     surname: req.body.surname,
@@ -22,21 +22,21 @@ router.post("/", (req, res) => {
     endTime: req.body.endTime,
     startTime: req.body.startTime,
     endDate: req.body.endDate,
-    status: "รอดำเนินการ",
-    approve: null,
-  });
+    status: 'รอดำเนินการ',
+    approve: null
+  })
 
   schedule
     .save()
-    .then(() => res.json("Save Succesfuly!"))
-    .catch((err) => res.status(400).json(`error:${err}`));
-});
+    .then(() => res.json('Save Succesfuly!'))
+    .catch(err => res.status(400).json(`error:${err}`))
+})
 
-router.put("/:id", (req, res) => {
+router.put('/:id', (req, res) => {
   scheduleModel
     .findById(req.params.id)
-    .then((data) => {
-      (data.status = req.body.status), (data.approve = req.body.approve);
+    .then(data => {
+      ;(data.status = req.body.status), (data.approve = req.body.approve)
 
       data
         .save()
@@ -48,55 +48,87 @@ router.put("/:id", (req, res) => {
             data.approve
           )
         )
-        .then(() => res.json("Succesfuly!"))
-        .catch((err) => res.status(400).json(`error:${err}`));
+        .then(() => res.json('Succesfuly!'))
+        .catch(err => res.status(400).json(`error:${err}`))
     })
-    .catch((err) => res.status(400).json(`error:${err}`));
-});
+    .catch(err => res.status(400).json(`error:${err}`))
+})
 
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
   scheduleModel
     .findById(req.params.id)
-    .then((data) => res.json(data))
-    .catch((err) => res.status(400).json(`error:${err}`));
-});
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json(`error:${err}`))
+})
 
-router.delete("/:id", (req, res) => {
+router.delete('/:id', (req, res) => {
   scheduleModel
     .findByIdAndDelete(req.params.id)
-    .then(() => res.json("Delete!"))
-    .catch((err) => res.status(400).json(`error:${err}`));
-});
+    .then((data) => {sendRejectNoti(data.surname, req.body.reason)})
+    .then(() => res.json('Delete!'))
+    .catch(err => res.status(400).json(`error:${err}`))
+})
 
-function sendnotification(name, department, type, approve) {
-  var sendData = `ชื่อผู้จอง : ${name}\n แผนก: ${department}\n ประเภทรถ: ${type}\n คนอนุมัติ: ${approve}\n ตรวจสอบสถานะรายการจอง: https://driver-project-40da5.web.app/bookingReport `;
-  var token = "rZg0KIwbrn3aaOGzEZSN6paRfXL2FpJXMOe6Ad4qG2a";
-  var message = sendData;
+function sendnotification (name, department, type, approve) {
+  var sendData = `ชื่อผู้จอง : ${name}\n แผนก: ${department}\n ประเภทรถ: ${type}\n คนอนุมัติ: ${approve}\n ตรวจสอบสถานะรายการจอง: https://driver-project-40da5.web.app/bookingReport `
+  var token = 'rZg0KIwbrn3aaOGzEZSN6paRfXL2FpJXMOe6Ad4qG2a'
+  var message = sendData
 
-  console.log({ message });
+  console.log({ message })
 
   request(
     {
-      method: "POST",
-      uri: "https://notify-api.line.me/api/notify",
+      method: 'POST',
+      uri: 'https://notify-api.line.me/api/notify',
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+        'Content-Type': 'application/x-www-form-urlencoded'
       },
       auth: {
-        bearer: token,
+        bearer: token
       },
       form: {
-        message: message,
-      },
+        message: message
+      }
     },
     (err, httpResponse) => {
       if (err) {
-        console.log(err);
+        console.log(err)
       } else {
-        console.log("send notification");
+        console.log('send notification')
       }
     }
-  );
+  )
 }
 
-module.exports = router;
+function sendRejectNoti (name, reason) {
+  var sendData = `ชื่อผู้จอง : ${name}\n คำขอไม่ได้รับการอนุมัติเนื่องจาก : ${reason}`
+  var token = 'rZg0KIwbrn3aaOGzEZSN6paRfXL2FpJXMOe6Ad4qG2a'
+  var message = sendData
+
+  console.log({ message })
+
+  request(
+    {
+      method: 'POST',
+      uri: 'https://notify-api.line.me/api/notify',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      auth: {
+        bearer: token
+      },
+      form: {
+        message: message
+      }
+    },
+    (err, httpResponse) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('send notification')
+      }
+    }
+  )
+}
+
+module.exports = router
